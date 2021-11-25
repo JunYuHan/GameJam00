5e6bfaefa5a0539d1fd459cc88b2a6d0219263f3
Choi <csy050420@gmail.com>
2021-11-25 15:16:46 +0900

    ll

---
+++ Assets/Scripts/Game20Scene.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game20Scene : GameManager
+{
+    int[] Dice = { 0, 0 };
+    bool firstLan1 = true, firstLan2 = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
