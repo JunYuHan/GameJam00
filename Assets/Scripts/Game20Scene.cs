a550d16ac9aa2ba054369fa4ed364863524c83ea
Choi <csy050420@gmail.com>
2021-11-25 16:05:23 +0900

    3

--- Assets/Scripts/Game20Scene.cs
+++ Assets/Scripts/Game20Scene.cs
@@ -1,22 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game20Scene : GameManager
 {
-    int[] Dice = { 0, 0 };
+    float[] Dice = { 0, 0 };
     bool firstLan1 = true, firstLan2 = true;
+
+    public Button Dice_btn;
+    public Text Dice_Txt;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (firstLan1 == true && firstLan2 == true)
+		{
+
+		}
+		else
+		{
+
+		}
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Dice_btn != null)
+            Dice_btn.onClick.AddListener(Dice_reroll);
     }
-    
+
+    void Dice_reroll()
+	{
+        
+	}
+
 }
