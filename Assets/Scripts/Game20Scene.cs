395bd6079cad98524d5b876c0d64f3bd89bbc786
Choi <csy050420@gmail.com>
2021-11-26 20:57:36 +0900

    기능반에서 작업

--- Assets/Scripts/Game20Scene.cs
+++ Assets/Scripts/Game20Scene.cs
@@ -5,11 +5,14 @@ using UnityEngine.UI;
 
 public class Game20Scene : GameManager
 {
+    #region
     public Button Back_Btn;
     public Button DiceTrun_Btn;
     public Button FourDice_Btn;
     public Button SixDice_Btn;
     public Button TwelveDice_Btn;
+    public Button Choose_Yes_Btn;
+    public Button Choose_No_Btn;
     public Text Player1_Score_Txt, Player2_Score_Txt;
     public Text End_Txt;
     public Text Choose_Txt;
@@ -24,8 +27,10 @@ public class Game20Scene : GameManager
     int[] four2 = new int[4];
     int[] six2 = new int[6];
     int[] twelve2 = new int[12];
-    int Player1_GameTurn = 6, Player2_GameTurn = 6;
+    int Player1_GameTurn = 0, Player2_GameTurn = 0;
     bool firstLan1 = true, firstLan2 = true;
+    #endregion
+    //변수
 
     // Start is called before the first frame update
     void Start()
@@ -36,15 +41,15 @@ public class Game20Scene : GameManager
     // Update is called once per frame
     void Update()
     {
-        //if ((player1_hap == 20 || player2_hap == 20) || (player1_fin == true && player2_fin == true)
-        //    || player1_hap + player2_hap == 0)
-        //{
+        if ((Player1_GameTurn >= 7 && Player2_GameTurn >= 7) || (player1_hap == 20 || player1_hap == 20))
+        {
 
-        //}
-        //else
-        //{
-        //    End_Pnl.SetActive(true);
-        //}
+
+            End_Pnl.SetActive(true);
+            End_Txt.text = "가 승리하였습니다.\n아무데나 클릭하면 타이틀로 돌아갑니다.";
+            rest();
+
+        }
 
   //      if (Back_Btn != null)
 		//{
@@ -55,6 +60,7 @@ public class Game20Scene : GameManager
 
     public void DiceTurn()
 	{
+        #region
         int Dummy01 = player1_hap * 1;
         int Dummy02 = player1_hap * 3;
         int Dummy03 = player1_hap / 2;
@@ -70,6 +76,8 @@ public class Game20Scene : GameManager
         int Dummy12 = player2_hap + 5;
         int Dummy13 = player2_hap + 6;
         int Dummy14 = player2_hap - 6;
+        #endregion 
+        //더미 변수
 
         if (firstLan1 == true || firstLan2 == true)
         {
@@ -144,7 +152,7 @@ public class Game20Scene : GameManager
                     player1_hap = twelve1[Random.Range(0, 12)];
                 }
 
-
+                Hap();
 
             }
             else if (player2_turn == true)
@@ -167,62 +175,130 @@ public class Game20Scene : GameManager
                     player2_hap = twelve2[Random.Range(0, 12)];
                 }
 
-                
+                Hap();
 
             }
+
         }
 
 	}
 
     void Hap()
 	{
-        if (firstLan1 == true || firstLan2 == true)
+        if (player1_turn == true && player2_turn == false)
         {
-            if (player1_turn == true && player2_turn == false)
-			{
-                player1_hap += (Dice[0] + Dice[1]);
-                player1_turn = false;
-                firstLan1 = false;
-                Player1_GameTurn--;
-                player2_turn = true;
-
-			}
-            else
-			{
-                player2_hap += (Dice[0] + Dice[1]);
-                player2_turn = false;
-                firstLan2 = false;
-                Player2_GameTurn--;
-                player1_turn = true;
-			}
-
+            player1_turn = false;
+            Player1_Score_Txt.text = "Player1_Score : " + player1_hap;
+            player2_turn = true;
+            Player1_GameTurn++;
         }
-        else
+        else if (player1_turn == false && player2_turn == true)
         {
-            if (player1_turn == true && player2_turn == false)
-			{
-                player1_hap += Dice[0];
-                player1_turn = false;
-                Player1_GameTurn--;
-                player2_turn = true;
-			}
-			else
-			{
-                player2_hap += Dice[0];
-                player2_turn = false;
-                Player2_GameTurn--;
-                player1_turn = true;
-			}
-
+            player2_turn = false;
+            Player2_Score_Txt.text = "Player2_Score : " + player2_hap;
+            player1_turn = true;
+            Player2_GameTurn++;
         }
 
     }
 
     void NextDice()
 	{
-        
+        Choose_Pnl.SetActive(true);
 	}
 
+    public void Choose_Yes()
+    {
+        #region
+        int Dummy01 = player1_hap * 1;
+        int Dummy02 = player1_hap * 3;
+        int Dummy03 = player1_hap / 2;
+        int Dummy04 = player1_hap * 2;
+        int Dummy05 = player1_hap + 5;
+        int Dummy06 = player1_hap + 6;
+        int Dummy07 = player1_hap - 6;
+
+        int Dummy08 = player2_hap * 1;
+        int Dummy09 = player2_hap * 3;
+        int Dummy10 = player2_hap / 2;
+        int Dummy11 = player2_hap * 2;
+        int Dummy12 = player2_hap + 5;
+        int Dummy13 = player2_hap + 6;
+        int Dummy14 = player2_hap - 6;
+        #endregion
+        //더미 변수
+
+        Choose_Pnl.SetActive(false);
+
+        if (player1_turn == true && player2_turn == false)
+        {
+            if (Dice_Choose == 1)
+            {
+                int[] four1 = { Dummy01, Dummy01, player1_hap + 3, player1_hap - 3 };
+                player1_hap = four1[Random.Range(0, 4)];
+            }
+            else if (Dice_Choose == 2)
+            {
+                int[] six1 = { Dummy01, Dummy02, player1_hap + 4, player1_hap - 4, Dummy03, Dummy03 };
+                player1_hap = six1[Random.Range(0, 6)];
+            }
+            else if (Dice_Choose == 3)
+            {
+                int[] twelve1 = { Dummy04, Dummy04, Dummy02, player1_hap * 4,
+                        Dummy05, Dummy05, Dummy06, Dummy06, Dummy07, Dummy07,
+                        Dummy03, Dummy03, player1_hap / 3, player1_hap / 4 };
+                player1_hap = twelve1[Random.Range(0, 12)];
+            }
+
+            firstLan1 = false;
+            player1_turn = false;
+            player2_turn = true;
+            Player1_GameTurn++;
+        }
+        else if (player1_turn == false && player2_turn == true)
+        {
+            if (Dice_Choose == 1)
+            {
+                int[] four2 = { Dummy08, Dummy08, player2_hap + 3, player2_hap - 3 };
+                player2_hap = four1[Random.Range(0, 4)];
+            }
+            else if (Dice_Choose == 2)
+            {
+                int[] six2 = { Dummy08, Dummy09, player2_hap + 4, player2_hap - 4, Dummy10, Dummy10 };
+                player2_hap = six2[Random.Range(0, 6)];
+            }
+            else if (Dice_Choose == 3)
+            {
+                int[] twelve2 = { Dummy11, Dummy11, Dummy09, player2_hap * 4,
+                        Dummy12, Dummy12, Dummy13, Dummy13, Dummy14, Dummy14,
+                        Dummy10, Dummy10, player2_hap / 3, player2_hap / 4 };
+                player2_hap = twelve2[Random.Range(0, 12)];
+            }
+
+            firstLan2 = false;
+            player2_turn = false;
+            player1_turn = true;
+            Player2_GameTurn++;
+        }
+    }
+    public void Choose_No()
+    {
+        Choose_Pnl.SetActive(false);
+        if (player1_turn == true && player2_turn == false)
+        {
+            firstLan1 = false;
+            player1_turn = false;
+            player2_turn = true;
+            Player1_GameTurn++;
+        }
+        else
+        {
+            firstLan2 = false;
+            player2_turn = false;
+            player1_turn = true;
+            Player2_GameTurn++;
+        }
+    }
     public void FourDice()
 	{
         Dice_Choose = 1;
