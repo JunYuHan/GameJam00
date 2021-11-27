1eb4d02d69d4dcb8c3171eed3d00eb2cbfa6b0e0
ChoiSou0 <csy050420@gmail.com>
2021-11-27 22:19:10 +0900

    20게임 완성

--- Assets/Scripts/Game20Scene.cs
+++ Assets/Scripts/Game20Scene.cs
@@ -13,11 +13,14 @@ public class Game20Scene : GameManager
     public Button TwelveDice_Btn;
     public Button Choose_Yes_Btn;
     public Button Choose_No_Btn;
+    public Button Stop_Btn;
     public Text Player1_Score_Txt, Player2_Score_Txt;
     public Text End_Txt;
     public Text Choose_Txt;
     public GameObject End_Pnl;
     public GameObject Choose_Pnl;
+
+    GameManager hap = new GameManager();
 
     public int Dice_Choose = 0;
     int[] Dice = { 0, 0 };
@@ -29,6 +32,9 @@ public class Game20Scene : GameManager
     int[] twelve2 = new int[12];
     int Player1_GameTurn = 0, Player2_GameTurn = 0;
     bool firstLan1 = true, firstLan2 = true;
+    public bool player1_Stop = false, player2_Stop = false;
+
+    public int result01 = 0, result02 = 0;
     #endregion
     //변수
 
@@ -41,14 +47,19 @@ public class Game20Scene : GameManager
     // Update is called once per frame
     void Update()
     {
-        if ((Player1_GameTurn >= 7 && Player2_GameTurn >= 7) || (player1_hap == 20 || player1_hap == 20))
+        if (player1_Stop == true)
+            player1_turn = false;
+        if (player2_Stop == true)
+            player2_turn = false;
+
+        if ((Player1_GameTurn >= 7 && Player2_GameTurn >= 7) || (player1_hap == 20 || player1_hap == 20)
+            || (player1_Stop == true && player2_Stop == true))
         {
-
-
             End_Pnl.SetActive(true);
-            End_Txt.text = "가 승리하였습니다.\n아무데나 클릭하면 타이틀로 돌아갑니다.";
-            rest();
 
+            Result();
+
+            
         }
 
   //      if (Back_Btn != null)
@@ -207,6 +218,36 @@ public class Game20Scene : GameManager
         Choose_Pnl.SetActive(true);
 	}
 
+    void Result()
+    {
+        
+
+        if (player1_hap < 0)
+            result01 = 20 - player1_hap;
+        else if (player1_hap > 20)
+            result01 = 20 + player1_hap;
+
+        if (player2_hap < 0)
+            result02 = 20 - player2_hap;
+        else if (player2_hap > 20)
+            result02 = 20 + player2_hap;
+
+        if (result01 == result02)
+            {
+                End_Txt.text = "무승부입니다.";
+            }
+            else if (result01 < result02)
+            {
+                End_Txt.text = "player1이 승리하였습니다.\nBack버튼을 클릭하면 \n타이틀로 돌아갑니다.";
+            }
+            else if (result01 > result02)
+            {
+                End_Txt.text = "player2이 승리하였습니다.\nBack버튼을 클릭하면 \n타이틀로 돌아갑니다.";
+            }
+
+
+    }
+
     public void Choose_Yes()
     {
         #region
@@ -297,6 +338,21 @@ public class Game20Scene : GameManager
             player2_turn = false;
             player1_turn = true;
             Player2_GameTurn++;
+        }
+    }
+    public void Stop()
+    {
+        if (player1_turn == true && player2_turn == false)
+        {
+            player1_turn = false;
+            player2_turn = true;
+            player1_Stop = true;
+        }
+        else if (player1_turn == false && player2_Stop == true)
+        {
+            player2_turn = false;
+            player1_turn = true;
+            player2_Stop = true;
         }
     }
     public void FourDice()
