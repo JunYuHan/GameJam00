d943e2062bae55a0bcba9a5d3183c4701836a6fe
junyuhan <sdh20200225@sdh.hs.kr>
2021-12-03 13:04:09 +0900

    d

--- Assets/Scripts/Main/PopUpSystem.cs
+++ Assets/Scripts/Main/PopUpSystem.cs
@@ -7,128 +7,130 @@ using UnityEngine.SceneManagement;
 
 public class PopUpSystem : MonoBehaviour
 {
-	Animator anim;
-	[SerializeField] GameObject PopUp;
-	[SerializeField] GameObject start;
-	[SerializeField] GameObject Credit;
-	[SerializeField] GameObject HowtoPlayPopUp1;
-	[SerializeField] GameObject HowtoPlayPopUp2;
-	[SerializeField] GameObject HowtoPlayPopUp3;
-	[SerializeField] GameObject HowtoPlayPopUp4;
-	[SerializeField] GameObject CreditClose;
- 	[SerializeField] GameObject HideImg; //Start 눌렀을 때 다른거 못 건들게
-	[SerializeField] GameObject SettingPopUp;
-	[SerializeField] GameObject HideImage; //Setting 창 눌렀을 때 다른거 못 건들게
+    Animator anim;
+    [SerializeField] GameObject PopUp;
+    [SerializeField] GameObject Test;
+    [SerializeField] GameObject start;
+    [SerializeField] GameObject Credit;
+    [SerializeField] GameObject HowtoPlayPopUp1;
+    [SerializeField] GameObject HowtoPlayPopUp2;
+    [SerializeField] GameObject HowtoPlayPopUp3;
+    [SerializeField] GameObject HowtoPlayPopUp4;
+    [SerializeField] GameObject CreditClose;
+    [SerializeField] GameObject HideImg; //Start 눌렀을 때 다른거 못 건들게
+    [SerializeField] GameObject SettingPopUp;
+    [SerializeField] GameObject HideImage; //Setting 창 눌렀을 때 다른거 못 건들게
 
-	[SerializeField] Image MusicImage;
-	[SerializeField] Sprite MusicSpriteOn;
-	[SerializeField] Sprite MusicSpriteOff;
+    [SerializeField] Image MusicImage;
+    [SerializeField] Sprite MusicSpriteOn;
+    [SerializeField] Sprite MusicSpriteOff;
 
-	AudioSource source;
-	private void Start()
-	{
-		HideImage.SetActive(false);
-		PopUp.SetActive(false);
-		SettingPopUp.SetActive(false);
-		anim = GetComponent<Animator>();
-		source = Camera.main.GetComponent<AudioSource>();
-	}
-
-	void Update()
-	{
-		//if (Input.GetMouseButtonDown(0))
-		//{
-		//	OnClickStartButton(true);
-		//}
-	}
-
-	public void OnClickStartButton(bool active) //플레이 버튼 눌렀을 때
-	{
-		HideImg.SetActive(active);
-		PopUp.SetActive(active);
-	}
-	public void OnClickSettingButton(bool active)	//Setting Ui눌렀을 때
-	{
-		SettingPopUp.SetActive(active);
-		HideImage.SetActive(active);
-	}
-	public void OnClickCloseButton(bool active) //세팅 버튼 x 눌렀을 때
+    AudioSource source;
+    private void Start()
     {
-		SettingPopUp.SetActive(false);
+        HideImage.SetActive(false);
+        PopUp.SetActive(false);
+        SettingPopUp.SetActive(false);
+        anim = GetComponent<Animator>();
+        source = Camera.main.GetComponent<AudioSource>();
     }
 
-	public void OnClickOnButton() //사운드 on 눌렀을 때
+    void Update()
     {
-		source.mute = false;
-		MusicImage.sprite = MusicSpriteOn;
-	}
-	public void OnClickOffButton() //사운드 off눌렀을 때
-	{
-		source.mute = true;
-		MusicImage.sprite = MusicSpriteOff;
-	}
-	
-	public void MoveScene(string sceneName) //씬 이동
-	{
-		SceneManager.LoadScene(sceneName);
-	}
-	public void OnClickExitButton()
-	{
-		Application.Quit();
-	}
-	public void OnClickHowtoPlay(bool active)
-	{
-		HowtoPlayPopUp1.SetActive(active);
-	}
-	public void OnClickXButton()
-	{
-		HowtoPlayPopUp1.SetActive(false);
-		HowtoPlayPopUp2.SetActive(false);
-		HowtoPlayPopUp3.SetActive(false);
-		HowtoPlayPopUp4.SetActive(false);
-	}
-	public void OnClickNextButton1()
-	{
-		HowtoPlayPopUp1.SetActive(false);
-		HowtoPlayPopUp2.SetActive(true);
-		HowtoPlayPopUp3.SetActive(false);
-		HowtoPlayPopUp4.SetActive(false);
-	}
-	public void OnClickNextButton1_B()
-	{
-		HowtoPlayPopUp1.SetActive(true);
-		HowtoPlayPopUp2.SetActive(false);
-		HowtoPlayPopUp3.SetActive(false);
-		HowtoPlayPopUp4.SetActive(false);
-	}
-	public void OnClickNextButton2_B()
-	{
-		HowtoPlayPopUp1.SetActive(false);
-		HowtoPlayPopUp2.SetActive(true);
-		HowtoPlayPopUp3.SetActive(false);
-		HowtoPlayPopUp4.SetActive(false);
-	}
-	public void OnClickNextButton3_B()
-	{
-		HowtoPlayPopUp1.SetActive(false);
-		HowtoPlayPopUp2.SetActive(false);
-		HowtoPlayPopUp3.SetActive(true);
-		HowtoPlayPopUp4.SetActive(false);
-	}
-	public void OnClickNextButton2()
-	{
-		HowtoPlayPopUp1.SetActive(false);
-		HowtoPlayPopUp2.SetActive(false);
-		HowtoPlayPopUp3.SetActive(true);
-		HowtoPlayPopUp4.SetActive(false);
-	}
-	public void OnClickNextButton3()
-	{
-		HowtoPlayPopUp1.SetActive(false);
-		HowtoPlayPopUp2.SetActive(false);
-		HowtoPlayPopUp3.SetActive(false);
-		HowtoPlayPopUp4.SetActive(true);
-	}
+            if (EventSystem.current.IsPointerOverGameObject() == false)
+            {
+                //Test.SetActive(false);
+                Debug.Log("ASDASD");
+            }
+    }
+
+    public void OnClickStartButton(bool active) //플레이 버튼 눌렀을 때
+    {
+        HideImg.SetActive(active);
+        PopUp.SetActive(active);
+    }
+    public void OnClickSettingButton(bool active)   //Setting Ui눌렀을 때
+    {
+        SettingPopUp.SetActive(active);
+        HideImage.SetActive(active);
+    }
+    public void OnClickCloseButton(bool active) //세팅 버튼 x 눌렀을 때
+    {
+        SettingPopUp.SetActive(false);
+    }
+
+    public void OnClickOnButton() //사운드 on 눌렀을 때
+    {
+        source.mute = false;
+        MusicImage.sprite = MusicSpriteOn;
+    }
+    public void OnClickOffButton() //사운드 off눌렀을 때
+    {
+        source.mute = true;
+        MusicImage.sprite = MusicSpriteOff;
+    }
+
+    public void MoveScene(string sceneName) //씬 이동
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void OnClickExitButton()
+    {
+        Application.Quit();
+    }
+    public void OnClickHowtoPlay(bool active)
+    {
+        HowtoPlayPopUp1.SetActive(active);
+    }
+    public void OnClickXButton()
+    {
+        HowtoPlayPopUp1.SetActive(false);
+        HowtoPlayPopUp2.SetActive(false);
+        HowtoPlayPopUp3.SetActive(false);
+        HowtoPlayPopUp4.SetActive(false);
+    }
+    public void OnClickNextButton1()
+    {
+        HowtoPlayPopUp1.SetActive(false);
+        HowtoPlayPopUp2.SetActive(true);
+        HowtoPlayPopUp3.SetActive(false);
+        HowtoPlayPopUp4.SetActive(false);
+    }
+    public void OnClickNextButton1_B()
+    {
+        HowtoPlayPopUp1.SetActive(true);
+        HowtoPlayPopUp2.SetActive(false);
+        HowtoPlayPopUp3.SetActive(false);
+        HowtoPlayPopUp4.SetActive(false);
+    }
+    public void OnClickNextButton2_B()
+    {
+        HowtoPlayPopUp1.SetActive(false);
+        HowtoPlayPopUp2.SetActive(true);
+        HowtoPlayPopUp3.SetActive(false);
+        HowtoPlayPopUp4.SetActive(false);
+    }
+    public void OnClickNextButton3_B()
+    {
+        HowtoPlayPopUp1.SetActive(false);
+        HowtoPlayPopUp2.SetActive(false);
+        HowtoPlayPopUp3.SetActive(true);
+        HowtoPlayPopUp4.SetActive(false);
+    }
+    public void OnClickNextButton2()
+    {
+        HowtoPlayPopUp1.SetActive(false);
+        HowtoPlayPopUp2.SetActive(false);
+        HowtoPlayPopUp3.SetActive(true);
+        HowtoPlayPopUp4.SetActive(false);
+    }
+    public void OnClickNextButton3()
+    {
+        HowtoPlayPopUp1.SetActive(false);
+        HowtoPlayPopUp2.SetActive(false);
+        HowtoPlayPopUp3.SetActive(false);
+        HowtoPlayPopUp4.SetActive(true);
+    }
 
 
 }
